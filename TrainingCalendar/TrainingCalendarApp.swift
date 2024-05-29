2034666a313c20f8e390fab3ef807a0c288383b0
visaeva <visaeva0302@gmail.com>
2024-05-29 18:49:05 +0300

    Initial Commit

---
+++ TrainingCalendar/TrainingCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingCalendarApp.swift
+//  TrainingCalendar
+//
+//  Created by Victoria Isaeva on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainingCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
